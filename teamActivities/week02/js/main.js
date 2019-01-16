aef977c27a0356f5eff2d2d465708f5870935ef3
TAP <tapetersen966@gmail.com>
2019-01-16 14:53:57 -0700

    Better jquery functionality added

--- teamActivities/week02/js/main.js
+++ teamActivities/week02/js/main.js
@@ -4,9 +4,23 @@ function message() {
 
 function colorChange() {
     //document.getElementById("div1").style.background = //document.getElementById("colorPicker").value;
-    $('.div1').css('background', document.getElementById("colorPicker").value);
+    var input = $('#colorPicker').val();
+    $('.div1').css('background', input);
+                   
 }
 
 function fade() {
     $('.div3').fadeOut("slow");
 }
+
+$(document).ready(function() {
+       $('#colorButton').click(function() {
+           colorChange();
+       });  
+});
+
+$(document).ready(function() {
+       $('#fade').click(function() {
+           fade();
+       });  
+});
