5a7fa320a926ee2d74060bbb01c81a709267d089
TAP <tapetersen966@gmail.com>
2019-01-19 13:28:36 -0700

    added php files and new folders for organization

--- web/index.php
+++ web/index.php
@@ -1,5 +1,87 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Travis Petersen's Home Page</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    <body>
+        <header>
+            <h1>Welcome to Travis Petersen's Home Page</h1>            
+            
+            <a href="../teamActivities/index.php">Team Activities</a>
+            <a href="assignments.php">Assignments</a>
+            
+            <hr />
+        </header>
+        
+        <main>
+            <div>
+                <ul>
+                    <li style="font-family: cursive">
+                        <strong style="font-family: sans-serif;">Name:</strong> Travis
+                        Petersen</li>
+                    <li style="font-family: monospace">
+                        <strong style="font-family: sans-serif;">Major:</strong> Web Design 
+                        and Development</li>
+                    <li style="font-family: cursive">
+                        <strong style="font-family: sans-serif;">From:</strong> Fairfield, 
+                        California</li>
+                </ul>
+                <img src="images/bowserLeap.jpg" alt="funny small dog picture" />
+            </div>
+        
+            <table>
+                <tr>
+                    <th>Course #</th>
+                    <th>Course Decription</th>
+                    <th>Sec #</th>
+                    <th>Class Time</th>
+                    <th>Class Location</th>
+                </tr>
+                    <tr>
+                    <td>CIT 365</td>
+                    <td>This class goes over how to use the .Net 
+                        Framework and how to develop apps/programs using C#</td>
+                    <td>02</td>
+                    <td>N/A</td>
+                    <td>Online</td>
+                </tr>
+                <tr>
+                    <td>CS 213</td>
+                    <td>This class goes over the basics of web 
+                        engineering. Similar to the CIT Front End Web Development 
+                        class</td>
+                    <td>04</td>
+                    <td>N/A</td>
+                    <td>Online</td>
+                </tr>
+                <tr>
+                    <td>CS 313</td>
+                    <td>A more advance version of CS 213. This goes 
+                        over the server side of web engineering.</td>
+                    <td>02</td>
+                    <td>N/A</td>
+                    <td>Online</td>
+                </tr>
+                <tr>
+                    <td>ECEN 160</td>
+                    <td>This class goes over digital systems and 
+                        boolean algebra.</td>
+                    <td>04</td>
+                    <td>N/A</td>
+                    <td>Online</td>
+                </tr>
+            </table>
+        
+            <p>A quote from Paul Walker that I can really get behind:</p>
+            <blockquote cite="https://www.brainyquote.com/quotes/paul_walker_424421">
+                My philosophy is: If you can't have fun, there's no sense in doing it.
+            </blockquote>
+        </main>
+        
+        <footer>
+        
+        </footer>        
+    </body>
+</html>
