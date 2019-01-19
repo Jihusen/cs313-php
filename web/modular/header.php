19a664b4c00beb41e6ec0c8b78cd837a99c85f5e
TAP <tapetersen966@gmail.com>
2019-01-19 14:36:50 -0700

    Fixed modular links

--- web/modular/header.php
+++ web/modular/header.php
@@ -1,6 +1,6 @@
 <h1>Welcome to Travis Petersen's Home Page</h1>            
             
-<a href="../teamActivities/index.php">Team Activities</a>
+<a href="/cs313-php/teamActivities/index.php">Team Activities</a>
 <a href="assignments.php">Assignments</a>
             
 <hr />
